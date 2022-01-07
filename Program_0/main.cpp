d6ac2c3476abe725c7226b6d261a02a48a68f86c
Aleksandr <Girevik24@yandex.ru>
2022-01-07 05:51:42 +0300

    First commit - from command line

---
+++ Program_0/main.cpp
@@ -0,0 +1,24 @@
+ 
+// Нужен для _getch()
+#include <conio.h>
+
+#include <iostream>
+#include <fstream>
+#include <limits>
+ 
+ 
+
+using namespace std;
+ 
+ 
+int main()
+{
+    cout << "Ave Caesar!" << endl;
+ 
+     
+ 
+    // Пауза после скрипта
+    _getch();
+
+    return 0;
+} 
